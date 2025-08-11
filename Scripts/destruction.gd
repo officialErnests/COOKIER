9986517f480bd7414aa7e7c676d18c79a8304d91
n0o0b090lv <ernestspursw@gmail.com>
2025-08-11 16:56:44 +0300

    Fun

--- Scripts/destruction.gd
+++ Scripts/destruction.gd
@@ -2,7 +2,7 @@ extends TileMapLayer
 
 func destory(in_position, radius) -> void:
 	var t_position = Vector2i(round(in_position/16))
-	print(t_position, in_position)
+	# print(t_position, in_position)
 	var pos2 : Array[Vector2i] = []
 	for x in range(radius*2 + 3):
 		if get_cell_tile_data(t_position + Vector2i(x - radius - 1,radius + 1)):
@@ -17,5 +17,7 @@ func destory(in_position, radius) -> void:
 	for x in range(radius*2 + 3):
 		for y in range(radius*2 + 3):
 			erase_cell(t_position + Vector2i(x - radius - 1, y - radius - 1))
-	print(pos2)
 	set_cells_terrain_connect(pos2,0,0)
+
+func _ready() -> void:
+	destory(Vector2(0,0),2)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,29 +4,51 @@ var SPEED = 200
 var prev_velocity = 0
 var charge = false
 @export var ground : TileMapLayer
+@onready var camera = $MainCam
 
 func _physics_process(delta: float) -> void:
-	velocity.x += Input.get_axis("left","right") * delta * SPEED
-	velocity.y += delta * 100
+	velocity.y += delta * 500
 	rotation_degrees += velocity.length() * delta * 10 * (1 if velocity.x > 0 else -1)
 	charge = false
-	if is_on_floor() :
+	if is_on_ceiling():
+		if prev_velocity.y < -200:
+			ground.destory(position,1)
+		velocity.y = prev_velocity.y / 2 - 100
 		if Input.is_action_pressed("jump"):
-			velocity.y += -100
+			velocity.y -= 100
+	if is_on_floor():
+		velocity *= 0.99
+		velocity.x += Input.get_axis("left","right") * delta * SPEED
+		if prev_velocity.y > 200:
+			ground.destory(position + Vector2(0,-16),1)
+		if Input.is_action_pressed("jump"):
+			velocity.y = -300
 		if Input.is_action_pressed("down"):
 			velocity.x += 1000 * delta if velocity.x > 0 else -1000 * delta
 			velocity *= 0.98
-			$MainCam.position = velocity/20 * Vector2(randi_range(-1,1),randi_range(-1,1))
+			camera.position = velocity/20 * Vector2(randi_range(-1,1),randi_range(-1,1))
 			charge = true
 	else:
+		velocity *= 0.995
+		velocity.x += Input.get_axis("left","right") * delta * SPEED / 2
 		if Input.is_action_pressed("down"):
-			velocity.y = 1000
+			velocity.y += 50
 	if is_on_wall():
-		ground.destory(position,2)
-		velocity.y -= abs(prev_velocity.x)
+		if abs(prev_velocity.x) > 300:
+			ground.destory(position + Vector2(0,-16*1.5),1)
+			print(int(Input.get_axis("left","right")), getDirection(prev_velocity.x))
+			if Input.get_axis("left","right") == getDirection(prev_velocity.x) * -1:
+				velocity.x = -prev_velocity.x 
+			else:
+				velocity.x = prev_velocity.x 
+			# position.x -= velocity.x / 100
+		if Input.is_action_pressed("jump"):
+			velocity.y -= 10
 	# angular_velocity += Input.get_axis("left","right") * delta * SPEED / 10
-	velocity *= 0.99
 	prev_velocity = velocity
 	if not charge:
-		$MainCam.position = Vector2(0,0)
+		camera.position = Vector2(0,0)
 		move_and_slide()
+
+func getDirection(i_position) -> int:
+	return 1 if i_position > 0 else 0 if i_position == 0 else -1
