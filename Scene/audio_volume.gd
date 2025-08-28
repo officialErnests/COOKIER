70de1b52b6381ed2dc5510833233000333cb169f
n0o0b090lv <ernestspursw@gmail.com>
2025-08-28 20:25:31 +0300

    Fixed audio bug, oops XD

--- Scene/audio_volume.gd
+++ Scene/audio_volume.gd
@@ -9,4 +9,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	volume_linear = CookLevel.volume * 0.5
 	CookLevel.audio_progresion = get_playback_position()
+	if !playing:
+		play()
 
